30fb21ed8a0b43f5d320b2fce22108d79eccc5cc
sidgroks@gmail.com <sidgroks@gmail.com>
2020-08-01 18:04:00 +0530

    add font and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import 'animate.css/animate.min.css';
-
 import '../styles/globals.css';
+import Head from 'components/head';
 
 function App({ Component, pageProps }) {
   return (
     <>
+      <Head />
       <Component {...pageProps} />
     </>
   );
